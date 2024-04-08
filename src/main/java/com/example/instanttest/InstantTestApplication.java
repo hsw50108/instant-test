5d48d9efb2fb10284410c664866f1f637f980e15
dongwook <hsw50108@gmail.com>
2024-04-08 17:29:13 +0900

    git init

---
+++ src/main/java/com/example/instanttest/InstantTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.instanttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/instanttest/InstantTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instanttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
